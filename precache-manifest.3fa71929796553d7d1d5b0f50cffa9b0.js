37b1ee12d5587fce6c813065940a5c1256bc24f7
Johnfg10 <5445550+johnfg10@users.noreply.github.com>
2020-01-25 06:57:46 +0000

    Updates

---
+++ precache-manifest.3fa71929796553d7d1d5b0f50cffa9b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c77093ca4f9030f08b72025d3096dc47",
+    "url": "/johngrayxyz/index.html"
+  },
+  {
+    "revision": "b7bf2590fa08a361f0fe",
+    "url": "/johngrayxyz/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f6b7cc31ac1b89b10c0c",
+    "url": "/johngrayxyz/static/js/2.eae2209b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/johngrayxyz/static/js/2.eae2209b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b7bf2590fa08a361f0fe",
+    "url": "/johngrayxyz/static/js/main.aa92ad90.chunk.js"
+  },
+  {
+    "revision": "ad8bfa230f953cde9bb9",
+    "url": "/johngrayxyz/static/js/runtime-main.2126694b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/johngrayxyz/static/media/logo.5d5d9eef.svg"
+  }
+]);
